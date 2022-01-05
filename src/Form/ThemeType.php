3dbce9fcdc8b9f4dc5be111bf32e656550d0bbc6
Matt Gibson <gothick@gothick.org.uk>
2022-01-05 16:30:22 +0000

    Validate start/end dates (start cannot be after end) and fix start/end date display in Twig templates.

--- src/Form/ThemeType.php
+++ src/Form/ThemeType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Theme;
+use DateTime;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -10,6 +11,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class ThemeType extends AbstractType
 {
